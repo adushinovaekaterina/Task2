925401419efb464c23191756c8cc11f50871d4bf
adushinova_ekaterina <adushinova2002@mail.ru>
2021-10-14 10:59:07 +0800

    Создала форму

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,56 @@ namespace Лабораторная_работа__2__задание_2_
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtOrder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOrder
+            // 
+            this.txtOrder.Location = new System.Drawing.Point(47, 43);
+            this.txtOrder.Name = "txtOrder";
+            this.txtOrder.Size = new System.Drawing.Size(105, 20);
+            this.txtOrder.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Последовательность цифр";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(357, 151);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOrder);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
